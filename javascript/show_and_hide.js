ae217313ff6b23aba5250c03d14c0ed56f723859
Galba177 <jannickkracht@googlemail.com>
2021-03-14 21:44:45 +0100

    s

--- javascript/show_and_hide.js
+++ javascript/show_and_hide.js
@@ -1,9 +1,9 @@
 function show_hide(id) {
-    var e = document.getElementById(id);
-    if (e.style.display === 'none') {
-        e.style.display = 'inline';
+    var bild = document.getElementById(id);
+    if (bild.style.display === 'none') {
+        bild.style.display = 'inline';
     } else {
-        e.style.display = 'none';
+        bild.style.display = 'none';
     }
 }
 
